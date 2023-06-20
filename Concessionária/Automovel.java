67a40bf2522b1c3530793570d419b8462240da81
Juliaferreir <101339279+Juliaferreira22@users.noreply.github.com>
2023-06-19 21:35:11 -0300

    Add files via upload

---
+++ Concessionária/Automovel.java
@@ -0,0 +1,26 @@
+//Classe de modelo
+public interface Automovel {
+
+    
+
+    public void cadastar();
+
+    public void listar();
+
+    public void atualiar();
+
+    public void excluir(); 
+
+   
+
+    
+    
+
+ 
+
+    
+
+   
+}
+
+
